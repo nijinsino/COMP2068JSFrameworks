634372bc782c3a5912a48f7a560fb89ef38a8ebb
Nijin Sino <nijinsino@gmail.com>
2025-09-23 16:36:05 -0400

    corrected math.random function

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -30,9 +30,41 @@ const schema = {
 // this will generate computer
 //  move based on random number
 function getComputerSelection() {
+    //random number
+    //  between 0.0 and 1.0
   const r = Math.random();
+ 
   if (r <= 0.34) return "PAPER";
   if (r <= 0.67) return "SCISSORS";
   return "ROCK";
 }
 
+// here this part of code 
+// decides winner using rock paper and scissors rule
+function decideWinner(user, comp) {
+  if (user === comp) return " Oops it's a tie";
+  const beats = { ROCK: "SCISSORS", SCISSORS: "PAPER", PAPER: "ROCK" };
+  return beats[user] === comp ? "User Wins" : "Computer Wins";
+}
+//main function
+async function main() {
+
+    //this will start the prompt
+  prompt.start();
+  try {
+    //this will ask user for an input and read their choice
+    const { userSelection } = await prompt.get(schema);
+    const user = userSelection.trim().toUpperCase();
+    const computer = getComputerSelection();
+//print both choice
+console.log(`\nUser chose: ${user}`);
+    console.log(`Computer chose: ${computer}`);
+    //display winner
+    console.log(`\n${decideWinner(user, computer)}`);
+  } catch (err) {
+    console.error("\nExiting...");
+    process.exit(1);
+  }
+}
+
+main();
