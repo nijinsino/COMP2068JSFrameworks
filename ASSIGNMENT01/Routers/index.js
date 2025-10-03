2881554a30e79704a8b9f46d7f0a6edc77d5345c
Nijin Sino <nijinsino@gmail.com>
2025-10-03 11:55:46 -0400

    created .hbs files

--- ASSIGNMENT01/Routers/index.js
+++ ASSIGNMENT01/Routers/index.js
@@ -1,9 +1,53 @@
+// here it will 
+// import Express 
+//  create a new Router object
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+// home page route
+router.get('/', (req, res) => 
+  res.render('index', { title: 'Home' })
+);
+
+// this is the about page route
+router.get('/about', (req, res) => 
+  res.render('about', { title: 'About Me' })
+);
+
+// projects page 
+router.get('/projects', (req, res) => 
+  res.render('projects', { title: 'Projects' })
+);
+
+// contact page route 
+router.get('/contact', (req, res) => 
+  res.render('contact', { title: 'Contact Me' })
+);
+
+// form
+// contact page
+router.post('/contact', express.urlencoded({ extended: false }), (req, res) => {
+
+  const { name, email, message } = req.body || {};
+
+  // this section is used to validate
+  // if there are any field missing
+  if (!name || !email || !message) {
+    return res.status(400).render('contact', {
+      title: 'Contact Me',
+      error: 'Please fill in all fields',
+      form: { name, email, message } 
+    });
+  }
+
+  //  show message if all fields are filled
+  res.render('contact', { 
+    title: 'Contact Me', 
+    flash: 'Message received, thank you!' 
+  });
 });
+
 
 module.exports = router;
