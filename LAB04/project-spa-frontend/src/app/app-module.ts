18676569332ea7f91db71415b830386f748511c3
Nijin Sino <nijinsino@gmail.com>
2025-11-07 15:58:33 -0500

    created project components

--- LAB04/project-spa-frontend/src/app/app-module.ts
+++ LAB04/project-spa-frontend/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Project } from './project/project';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Project
   ],
   imports: [
     BrowserModule,
